e6e85f2ddb8cfe0e151835d9073da93a9b3f0b2f
Raghu Saxena <poiasdpoiasd@live.com>
2024-02-09 16:53:01 +0800

    Disable tests

--- tests/sortCrops.test.ts
+++ tests/sortCrops.test.ts
@@ -1,7 +1,8 @@
-import { sortCropRecord } from '../src/utilities';
+// import { sortCropRecord } from '../src/utilities';
 
-const record = { '524': 1, '924': 1, '1080': 5 };
+// const record = { '524': 1, '924': 1, '1080': 5 };
 
-test('Sort record should return the maximum frequency value', () => {
-    expect(sortCropRecord(record)).toEqual(1080);
-});
+// test('Sort record should return the maximum frequency value', () => {
+//     expect(sortCropRecord(record)).toEqual(1080);
+// });
+// TODO: Fix tests with ava
